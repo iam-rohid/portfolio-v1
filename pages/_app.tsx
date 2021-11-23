d81944921b3386a94dbc63bcef3a6e0ae00d2248
Rohidul Islam <rohidul209@gmail.com>
2021-11-24 00:37:57 +0600

    Adsense code replaced

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import Background from "../components/Background";
 import Footer from "../components/Footer";
@@ -13,6 +14,13 @@ const MyApp = ({ Component, pageProps }) => {
       <Fragment>
         {/* <Background /> */}
         <div className="relative">
+          <Head>
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7322439099058988"
+              crossOrigin="anonymous"
+            ></script>
+          </Head>
           <NavBar />
           <Component {...pageProps} />
           <Footer />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,11 +17,6 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7322439099058988"
-          crossOrigin="anonymous"
-        ></script>
       </Html>
     );
   }
