2ea342ff430a12ce2c9c3256e2f78c977079b4a2
rohid-hub <rohidul209@gmail.com>
2022-01-26 22:56:51 +0600

    Google Analytics and Adsence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,61 @@
-import React, { Fragment } from "react";
-import Head from "next/head";
+import React, { Fragment, useEffect } from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import ThemeProvider from "../hooks/useTheme";
 import "tailwindcss/tailwind.css";
 import "../styles/index.scss";
 import "../styles/material-darker.css";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
 
 const MyApp = ({ Component, pageProps }) => {
-  return (
-    <ThemeProvider>
-      <Fragment>
-        <div className="relative">
-          <Head>
-            <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=G-6QZTGSP5LL"
-            ></script>
-            <script>
-              {`
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-6QZTGSP5LL');
-              `}
-            </script>
-            <script
-              async
-              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7322439099058988"
-              crossOrigin="anonymous"
-            ></script>
-          </Head>
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
+  return (
+    <Fragment>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7322439099058988"
+        crossOrigin="anonymous"
+      />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ThemeProvider>
+        <div className="relative">
           <NavBar />
           <div className="min-h-[95vh]">
             <Component {...pageProps} />
           </div>
           <Footer />
         </div>
-      </Fragment>
-    </ThemeProvider>
+      </ThemeProvider>
+    </Fragment>
   );
 };
 
